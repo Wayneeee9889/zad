8601b01c0087644cc17200de6d877cba537ab5e6
Waynee <uwusiara9889@gmail.com>
2026-03-03 10:33:57 +0100

    Add files via upload

---
+++ a.php
@@ -0,0 +1,19 @@
+<?php
+echo "=================================================================  \n";
+
+// Za pomocą pętli for wypisz na ekranie dowolne znaki, tak aby układały się w 2 wiersze po 10 znaków.
+
+for($i=0; $i<2; $i++){
+    echo "++++++++++ \n";
+}
+
+echo "=================================================================  \n";
+
+// Za pomocą pętli for, while i do… while napisz skrypt, który będzie zwiększał zmienną $i od 1 do 100 o 2, a następnie zmniejszał ją do 1 o 4.
+
+$i = 1;
+while($i == 100){
+    $i += 2;
+};
+
+echo "=================================================================  \n";
--- apopa.php
+++ apopa.php
@@ -1,26 +1,15 @@
 <?php
-// 5.Sprawdzający wiek.
-// Mniej niż 11: dziecko;
-// Między 11 i 17: nastolatek;
-// 18 i więcej: dorosły;
-
-// 6.Sprawdzający siłę hasła.
-// Mniej niż 5: bardzo słabe;
-// Między 5 i 8: słabe;
-// Między 9 i 11: silne;
-// Ponad 11: bardzo silne;
-
 // 1.Napisz program informujący czy liczba parzysta czy nieparzysta.
 
 $liczba1 = readline("podaj pierwszą liczbę: ");
 
 if ($liczba1 % 2 == 0){
-    echo "liczba jest parzysta                                                                                                              ";
+    echo "liczba jest parzysta \n";
 }else{
-    echo "liczba jest nieparzysta                                                                                                                                 ";
+    echo "liczba jest nieparzysta \n";
 };
 
-echo "=================================================================                                                                                ";
+echo "=================================================================  \n";
 
 // 2.Sprawdza czy pierwsza jest podzielna przez drugą.
 
@@ -28,30 +17,88 @@ $liczba2 = readline("podaj pierwszą liczbę: ");
 $liczba3 = readline("podaj druga liczbę: ");
 
 if($liczba2 % $liczba3 == 0){
-    echo "liczba " . $liczba2 . "jest pdzielna przez liczbę " . $liczba3;
+    echo "liczba " . $liczba2 . " jest pdzielna przez liczbę " . $liczba3 \n;
 }else{
-    echo "nie da sie podzielić liczbę " . $liczba2 . "przez liczbę " . $liczba3 . "                                                                                  ";
+    echo "nie da sie podzielić liczbę " . $liczba2 . "przez liczbę " . $liczba3 \n;
 };
 
-echo "=================================================================                                                                                   ";
+echo "=================================================================   ";
 
 // 3.Napisz program sprawdzający czy liczba jest z przedziału <1,10> lub <17,21>
 
 $liczba4 = readline("podaj pierwsza liczbę: ");
 
 if($liczba4 >=1 && $liczba4 <=10){
-    echo "jiczba jest w przedziale 1                                                                                              ";
+    echo "liczba jest w przedziale 1  \n";
 }else if($liczba4 >=17 && $liczba4 <=21){
-    echo "liczba jest w przedziale 2                                                                                            ";
+    echo "liczba jest w przedziale 2  \n";
 }else{
-    echo "liczba nie mieści się w żadnym przedziałów                                                                         ";
+    echo "liczba nie mieści się w żadnym przedziałów \n";
 }
 
-echo "=================================================================                                                                                   ";
+echo "================================================================= \n";
 
 // 4.Napisz program informujący czy liczba jest większa, mniejsza czy równa zero.
 // Wykorzystaj tylko jedną, podstawową instrukcję warunkową.
 
+$liczba5 = readline("podaj ");
 
+switch($liczba5){
+    case $liczba5 > 0:
+        echo "liczba jest większa od 0 \n";
+        break;
+    case $liczba5 = 0:
+        echo "liczba jest równa 0 \n";
+        break;
+    case $liczba5 < 0:
+        echo "leczba jest mniejsza od 0  \n";
+        break;
+};
 
+echo "=================================================================  \n";
 
+// 5.Sprawdzający wiek.
+// Mniej niż 11: dziecko;
+// Między 11 i 17: nastolatek;
+// 18 i więcej: dorosły;
+
+$wiek = readline("podaj swój wiek: ");
+
+switch($wiek){
+    case $wiek < 11:
+        echo "Jesteś dzieckiem  \n";
+        break;
+    case $wiek >= 11 && $wiek <= 17:
+        echo "Jesteś nastolatkiem  \n";
+        break;
+    case $wiek >= 18:
+        echo "jesteś dorosły  \n";
+        break;
+};
+
+echo "=================================================================  \n";
+
+// 6.Sprawdzający siłę hasła.
+// Mniej niż 5: bardzo słabe;
+// Między 5 i 8: słabe;
+// Między 9 i 11: silne;
+// Ponad 11: bardzo silne;
+
+$liczba6 = readline("podaj hasło dla sprawdzenia");
+
+switch($liczba6){
+    case strlen($liczba6) < 5:
+        echo "bardzo słabe! \n";
+    break;
+    case strlen($liczba6) >=5 && strlen($liczba6) <= 8:
+        echo "słabe \n";
+    break;
+    case strlen($liczba6) >= 9 && strlen($liczba6) <= 11:
+        echo "silne  \n";
+    break;
+    case strlen($liczba6) > 11:
+        echo "bardzo silne! \n";
+    break;
+}
+
+echo "=================================================================  \n";
