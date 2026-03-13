d9de2e822b7d7acaa9c5c9f4a266879e78f48748
Waynee <uwusiara9889@gmail.com>
2026-03-13 10:37:41 +0100

    Update apopa.php

--- apopa.php
+++ apopa.php
@@ -17,9 +17,9 @@ $liczba2 = readline("podaj pierwszą liczbę: ");
 $liczba3 = readline("podaj druga liczbę: ");
 
 if($liczba2 % $liczba3 == 0){
-    echo "liczba " . $liczba2 . " jest pdzielna przez liczbę " . $liczba3 \n;
+    echo "liczba " . $liczba2 . " jest pdzielna przez liczbę " . $liczba3 . "\n";
 }else{
-    echo "nie da sie podzielić liczbę " . $liczba2 . "przez liczbę " . $liczba3 \n;
+    echo "nie da sie podzielić liczbę " . $liczba2 . "przez liczbę " . $liczba3 . "\n";
 };
 
 echo "=================================================================   ";
@@ -102,3 +102,25 @@ switch($liczba6){
 }
 
 echo "=================================================================  \n";
+
+// Ilość dni w lutym.
+// Program zwraca ilość dni w lutym w zależności od roku przestępnego.
+// Lata przestępne liczymy od momentu reformy kalendarza w roku 1582.
+// Rokiem przestępnym jest ten, który jest podzielny przez 4.
+// Wyjątek stanowią lata podzielne przez 100 z wyjątkiem podzielnych przez 400.
+// Np.: rok 1900 nie był przestępny, ale 1600 lub 2000 już tak.
+
+$rok = readline("podaj rok króry chcesz sprawdzić: ");
+$dniWMiesiacu = "";
+
+if($rok % 4 == 0){
+    $dniWMiesiacu = "28";
+    echo("to jest rok przestępny! Ilość dni w miesiącu: " . $dniWMiesiacu);
+}else{
+    $dniWMiesiacu = "27";
+    echo("to nie jest rok przestępny! ilośc dni w miesiącu:  " . $dniWMiesiacu);
+}
+
+echo "=================================================================  \n";
+
+// Napisz program wyznaczający najmniejszą z trzech liczb podanych przez użytkownika.
